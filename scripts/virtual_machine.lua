e53a6eb36045aff0057095307cf06fc9871f1b84
Mobin <99535731+MobinYengejehi@users.noreply.github.com>
2022-10-18 19:24:15 +0330

    Update virtual_machine.lua

--- scripts/virtual_machine.lua
+++ scripts/virtual_machine.lua
@@ -1,6 +1,7 @@
 function CreateVirtualMachine()
     local machine = createElement("javascript-vm")
     if isElement(machine) then
+        SetElemnetData(machine,"stack",CreateStack())
         return machine
     end
 end
