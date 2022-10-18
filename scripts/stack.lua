513e605fda21a709d30ad569e513a03506fce6e6
Mad3in07 <116091305+Mad3in07@users.noreply.github.com>
2022-10-18 22:00:46 +0330

    Update stack.lua

--- scripts/stack.lua
+++ scripts/stack.lua
@@ -12,7 +12,7 @@ function CreateStack()
             
         end,
         tonumber = function()
-            -- check kon bbin in oke ya algorithm ro eshtab raftam
+            -- check kon bbin in oke ya algorithm ro eshtab raftam (Mad3in)
             local last_stack = stack[#stack-1]
             if(last_stack and type(last_stack.type) == "number") then
                 return last_stack.value
