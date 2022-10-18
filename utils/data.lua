c0214a69976aad2d8d628dd5e0fb0a48e6146d81
Mobin <99535731+MobinYengejehi@users.noreply.github.com>
2022-10-18 18:55:36 +0330

    Create data.lua

---
+++ utils/data.lua
@@ -0,0 +1,38 @@
+local Data = {}
+
+function SetElementData(element,key,value)
+    if IsElement(element) and key ~= nil then 
+        if type(Data[element]) ~= "table" then 
+            Data[element] = {}
+        end
+        if Data[element][key] ~= value then
+            Data[element][key] = value
+        end
+    end
+end
+
+function GetElementData(element,key)
+    if IsElement(element) and key ~= nil then 
+        if type(Data[element]) == "table" then 
+            return Data[element][key]
+        end
+    end
+end
+
+function RemoveElementData(element,key)
+    if IsElement(element) and key ~= nil then 
+        if type(Data[element]) == "table" then
+            Data[element][key] = nil
+            collectgarbage("collect")
+        end
+    end
+end
+
+function ClearElementData(element)
+    if IsElement(element) then 
+        if type(Data[element]) == "table" then 
+            Data[element] = nil
+            collectgarbage("collect")
+        end
+    end
+end
