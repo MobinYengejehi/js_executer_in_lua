03da50334b43158a1002c2614aefb1a6d5d48b7a
Mobin <99535731+MobinYengejehi@users.noreply.github.com>
2022-10-18 20:07:06 +0330

    Update stack.lua

--- scripts/stack.lua
+++ scripts/stack.lua
@@ -1,1 +1,79 @@
+function CreateStack()
+    local global = {}
+    local stack = {}
+    local refs = {}
+    return {
+        getglobal = function(name)
+            if type(name) == "string" then
+                -- nil
+            end
+        end,
+        next = function()
+            
+        end,
+        tonumber = function()
+            
+        end,
+        tostring = function()
+            
+        end,
+        toobject = function()
+            
+        end,
+        toarray = function()
+            
+        end,
+        toboolean = function()
+            
+        end,
+        tonil = function()
+            
+        end,
+        tofunction = function()
+            
+        end,
+        pushnumber = function()
+            
+        end,
+        pushstring = function()
+            
+        end,
+        pushobject = function()
+            
+        end,
+        pushboolean = function()
+            
+        end,
+        pushundefined = function()
+            
+        end,
+        pushnull = function()
+            
+        end,
+        pusharray = function()
+            
+        end,
+        pushfunction = function()
+            
+        end,
+        call = function(argCount,returnCount)
+            argCount = tonumber(argCount)
+            returnCount = tonumber(returnCount)
+            if argCount and returnCount then
+                 
+            end
+        end,
+        pop = function(amount)
+            amount = tonumber(amount)
+            if amount and amount > 0 then
+                for i = 1,amount do 
+                    table.remove(stack,#stack)
+                end
+            end
+        end
+    }
+end
 
+function CreateState()
+    
+end
