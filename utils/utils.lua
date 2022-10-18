7928564863bce13be5ab20a5934f638049e683ef
Mobin <99535731+MobinYengejehi@users.noreply.github.com>
2022-10-18 19:44:05 +0330

    Create utils.lua

---
+++ utils/utils.lua
@@ -0,0 +1,74 @@
+math.NaN = 0/0
+
+function RandomKey(amount)
+    amount = tonumber(amount) or 8
+    if amount then
+        local chars = "abcdefghijklmnoqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789-=_+~`!@#$%^&*()\\|/'\""
+        local key = ""
+        for i = 1,amount do 
+            key = key + chars[math.random(1,#chars)]
+        end
+        return key
+    end
+end
+
+function table.filter(tab,func,i)
+    if type(tab) == "table" and type(func) == "function" then
+        local result = {}
+        for k,v in (i and ipairs or pairs)(tab) do
+            local value = func(i,v)
+            if value ~= nil then
+                if i then
+                    table.insert(tab,value)
+                else
+                    result[k] = value
+                end
+            end
+        end
+        return result
+    end
+end
+
+function table.find(tab,value)
+    if type(tab) == "table" then
+        for k,v in pairs(tab) do
+            if v == value then
+                return k 
+            end
+        end
+    end
+end
+
+function table.empty(tab)
+    if type(tab) == "table" then
+        local result = true
+        for i,v in pairs(tab) do
+            if v ~= nil then
+                result = false
+                break
+            end
+        end
+        return result
+    end
+    return true
+end
+
+function table.size(tab)
+    if type(tab) == "table" then 
+        local size = 0
+        for _ in pairs(tab) do
+            size = size + 1
+        end
+        return size
+    end
+end
+
+function math.isNaN(x)
+    x = tonumber(x)
+    if x then
+        if tostring(x)%"nan" then
+            return true
+        end
+    end
+    return false
+end
