c2b9c34df92e7d44492c53973d2260c6f270f040
Mobin <99535731+MobinYengejehi@users.noreply.github.com>
2022-10-19 16:06:24 +0330

    Stack type definition added

--- scripts/stack.lua
+++ scripts/stack.lua
@@ -1,92 +1,168 @@
+GLOBALINDEX = 1
+UNDEFINED = enum{}
+NULL = enum{}
+
+LuaTypes = enum{
+    "Nothing",
+    Nil = "nil",
+    Boolean = "boolean",
+    String = "string",
+    Number = "number",
+    Table = "table",
+    Userdata = "userdata",
+    Function = "function"
+}
+
+VariableTypes = enum{
+    "Undefined",
+    "Null",
+    "Boolean",
+    "Number",
+    "String",
+    "Object",
+    "Function",
+    "Symbol"
+}
+
 function CreateStack()
     local global = {}
     local stack = {}
     local refs = {}
-    return {
-        getglobal = function(name)
-            if type(name) == "string" then
-                -- nil
-            end
-        end,
-        next = function()
-            
-        end,
-        tonumber = function()
-            local last_stack = stack[#stack-1]
-            if(last_stack and type(last_stack.type) == "number") then
-                return last_stack.value
-            end
-        end,
-        tostring = function()
-            local last_stack = stack[#stack-1]
-            if(last_stack and type(last_stack.type) == "string") then
-                return last_stack.value
-            end
-        end,
-        toobject = function()
-            local last_stack = stack[#stack-1]
-            if(last_stack and type(last_stack.type) == "object") then
-                return last_stack.value
-            end
-        end,
-        toarray = function()
-            
-        end,
-        toboolean = function()
-            
-        end,
-        tonil = function()
-            
-        end,
-        tofunction = function()
-            
-        end,
-        pushnumber = function(val)
-            if(val and type(val) == "number")then
-                table.insert(stack,{type = "number",value = val})
-            end
-        end,
-        pushstring = function(val)
-            if(val and type(val) == "string")then
-                table.insert(stack,{type = "string",value = val})
-            end
-        end,
-        pushobject = function()
-            
-        end,
-        pushboolean = function()
-            
-        end,
-        pushundefined = function()
-            
-        end,
-        pushnull = function()
-            
-        end,
-        pusharray = function()
-            
-        end,
-        pushfunction = function()
-            
-        end,
-        call = function(argCount,returnCount)
-            argCount = tonumber(argCount)
-            returnCount = tonumber(returnCount)
-            if argCount and returnCount then
-                 
-            end
-        end,
-        pop = function(amount)
-            amount = tonumber(amount)
-            if amount and amount > 0 then
-                for i = 1,amount do 
-                    table.remove(stack,#stack)
+    local dead = false
+    local function v_type(index)
+        if not dead then
+            index = tonumber(index)
+            if index then
+                if type(stack[index]) == LuaTypes.Table then
+                    return stack[index].type
                 end
             end
+        end
+    end
+    return {
+        type = v_type,
+        pushundefined = function()
+            if not dead then
+                table.insert(stack,{
+                    type = VariableTypes.Undefined,
+                    value = UNDEFINED
+                })
+            end
+        end,
+        pushnull = function()
+            if not dead then
+                table.insert(stack,{
+                    type = VariableTypes.Null,
+                    value = NULL
+                })
+            end
+        end,
+        pushboolean = function(value)
+            if not dead then
+                if type(value) == LuaTypes.Boolean then 
+                    table.insert(stack,{
+                        type = VariableTypes.Boolean,
+                        value = value
+                    })
+                end
+            end
+        end,
+        pushnumber = function(value)
+            if not dead then
+                if type(value) == LuaTypes.Number then
+                    table.insert(stack,{
+                        type = VariableTypes.Number,
+                        value = value
+                    })
+                end
+            end
+        end,
+        pushstring = function(value)
+            if not dead then
+                if type(value) == LuaTypes.String then
+                    table.insert(stack,{
+                        type = VariableTypes.String,
+                        value = value
+                    })
+                end
+            end
+        end,
+        pushfunction = function(value)
+
+        end,
+        tonil = function(index)
+            if not dead then
+                if (
+                    v_type(index) == VariableTypes.Null or
+                    v_type(index) == VariableTypes.Undefined
+                ) then
+                    return nil
+                end
+            end
+            return LuaTypes.Nothing
+        end,
+        toboolean = function(index)
+            if not dead then
+                if v_type(index) == VariableTypes.Boolean then 
+                    return stack[index].value
+                end
+            end
+            return LuaTypes.Nothing
+        end,
+        tonumber = function(index)
+            if not dead then 
+                if v_type(index) == VariableTypes.Number then
+                    return stack[index].value
+                end
+            end
+            return LuaTypes.Nothing
+        end,
+        tostring = function(index)
+            if not dead then
+                if v_type(index) == VariableTypes.String then
+                    return stack[index].value
+                end
+            end
+            return LuaTypes.Nothing
+        end,
+        pop = function(amount)
+            if not dead then
+                amount = tonumber(amount)
+                if amount and amount > 0 then
+                    for i = 1,amount do
+                        if #stack > 0 then
+                            table.remove(stack,#stack)
+                        else
+                            break
+                        end
+                    end
+                end
+            end
+        end,
+        alive = function()
+            return dead
+        end,
+        free = function()
+            global = nil
+            stack = nil
+            refs = nil
+            v_type = nil
+            dead = true
+            collectgarbage("collect")
         end
     }
 end
 
-function CreateState()
-    
-end
+local stack = CreateStack()
+
+stack.pushnull()
+stack.pushundefined()
+stack.pushboolean(true)
+stack.pushstring("salam chetori?")
+
+--stack.pop(1)
+
+local value = stack.tostring(4)
+
+
+print(value,value == LuaTypes.Nothing)
